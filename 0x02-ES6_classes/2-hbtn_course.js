4c0b81552158f44028e9e76cabc4df786822b378
Frank <kamunyuz@gmail.com>
2024-04-25 19:26:16 +0300

    getter and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,9 @@
-export default function HolbertonCourse () {
-    constructor (name, length, students) {
+export default class HolbertonCourse {
+    constructor(name, length, students) {
         if (typeof name !== 'string' ) {
             throw new  Error('Name must be a string');
         } else if (typeof length !== 'number') {
-            throw new Error('Length must be a number')
+            throw new Error('Length must be a number');
         } else if (!Array.isArray(students)) {
             throw new Error ('Students should be an array');
         }
