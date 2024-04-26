499be90575ade8013f08eab8c8381bf4a613ef50
Frank <kamunyuz@gmail.com>
2024-04-26 23:25:13 +0300

    currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   constructor(code, name) {
     if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
+      throw new TypeError('Code must be  a string');
     } else if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
@@ -10,19 +10,8 @@ export default class Currency {
     this._name = name;
   }
 
-  get name() {
-    return this._name;
-  }
-
   get code() {
     return this._code;
-  }
-
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = name;
   }
 
   set code(code) {
@@ -30,6 +19,17 @@ export default class Currency {
       throw new TypeError('Code must be a string');
     }
     this._code = code;
+  }
+
+  get name() {
+    return this._name = name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name;
   }
 
   displayFullCurrency() {
