edbf24cfc3547682e6b03004dc5cacf63d3e4d79
Frank <kamunyuz@gmail.com>
2024-05-03 17:58:48 +0300

    set array

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,3 +1,3 @@
-export default function(arr) {
+export default function setFromArray(arr) {
   return new set(arr);
 }
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,3 @@
-export default function hasValuesFromArray() {
+export default function hasValuesFromArray(set, array) {
   
 }
